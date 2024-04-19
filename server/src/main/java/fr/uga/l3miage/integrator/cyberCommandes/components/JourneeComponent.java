0f3065354c3f72d2b9e57990f518dbb8927a4325
l3miage-aragaooh <higo.aragao-oliveira-medeiros@etu.univ-grenoble-alpes.fr>
2024-04-19 14:25:59 +0200

    feat: implementation de delete par reference

--- server/src/main/java/fr/uga/l3miage/integrator/cyberCommandes/components/JourneeComponent.java
+++ server/src/main/java/fr/uga/l3miage/integrator/cyberCommandes/components/JourneeComponent.java
@@ -18,7 +18,12 @@ public class JourneeComponent {
         return journeeRepository.findAll();
     }
 
+<<<<<<< HEAD
     public JourneeEntity createJourneeEntity(JourneeEntity journeeEntity){
         return journeeRepository.save(journeeEntity) ;
+=======
+    public void deleteJourneeById(String reference){
+        journeeRepository.deleteById(reference);
+>>>>>>> 19a9ad5 (feat: implementation de delete par reference)
     }
 }
