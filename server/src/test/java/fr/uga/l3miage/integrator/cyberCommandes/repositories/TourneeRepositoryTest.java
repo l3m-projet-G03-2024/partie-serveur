5bf84c6b6eea4da30c666d5cfa42a498949d011c
abdramaa <abakar.abdraman@etu.univ-grenoble-alpes.fr>
2024-04-26 12:32:55 +0200

    test : modification des tests sur listing des tournees

--- server/src/test/java/fr/uga/l3miage/integrator/cyberCommandes/repositories/TourneeRepositoryTest.java
+++ server/src/test/java/fr/uga/l3miage/integrator/cyberCommandes/repositories/TourneeRepositoryTest.java
@@ -3,6 +3,7 @@ package fr.uga.l3miage.integrator.cyberCommandes.repositories;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.List;
 
+import fr.uga.l3miage.integrator.cyberCommandes.models.JourneeEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -17,17 +18,23 @@ public class TourneeRepositoryTest {
     @Autowired
     private TourneeRepository tourneeRepository;
 
+    @Autowired
+    private JourneeRepository journeeRepository;
+
     @Test
     void testRequestFindAllByEtat(){
         //Création d'un ensemble de tournées
+        JourneeEntity journee1 = JourneeEntity.builder().reference("01bc").build();
         TourneeEntity tourneeEntity = TourneeEntity
             .builder()
             .reference("1T")
+                .journee(journee1)
             .etat(EtatsDeTournee.ENCHARGEMENT)
             .build();
         TourneeEntity tourneeEntity2 = TourneeEntity
             .builder()
             .reference("2T")
+                .journee(journee1)
             .etat(EtatsDeTournee.ENCHARGEMENT)
             .build();
         TourneeEntity tourneeEntity3 = TourneeEntity
@@ -35,11 +42,12 @@ public class TourneeRepositoryTest {
             .reference("3T")
             .etat(EtatsDeTournee.EFFECTUEE)
             .build();
+        journeeRepository.save(journee1);
         tourneeRepository.save(tourneeEntity);
         tourneeRepository.save(tourneeEntity2);
         tourneeRepository.save(tourneeEntity3);
 
-        List<TourneeEntity> tourneeEntities = tourneeRepository.findAllByEtat(EtatsDeTournee.ENCHARGEMENT);
+        List<TourneeEntity> tourneeEntities = tourneeRepository.findAllByEtatOrJourneeReference(EtatsDeTournee.ENCHARGEMENT,journee1.getReference());
         
         // On s'attend à recevoir 2 tournées en chargement
         assertEquals( 2, tourneeEntities.size()); 
