ccfbea44275dd3c447ea4e279074899c58992d27
abdramaa <abakar.abdraman@etu.univ-grenoble-alpes.fr>
2024-05-03 14:50:34 +0200

    comment methods in CommandeServiceTest

--- server/src/test/java/fr/uga/l3miage/integrator/cyberVitrine/services/CommandeServiceTest.java
+++ server/src/test/java/fr/uga/l3miage/integrator/cyberVitrine/services/CommandeServiceTest.java
@@ -42,101 +42,101 @@ public class CommandeServiceTest {
     private LivraisonComponent livraisonComponent ;
 
 
-    @Test
-    void getCommandesWithNoState() {
-
-       // Given
-
-
-        ClientEntity client1 = new ClientEntity();
-        ClientEntity client2 = new ClientEntity();
-
-
-        CommandeEntity commande1 = new CommandeEntity(
-                "1ABC",
-                null,
-                null,
-                10,
-                "magnifique",
-                client1,
-                null,
-                null
-        );
-        CommandeEntity commande2 = new CommandeEntity(
-                "1BC",
-                null,
-                null,
-                9,
-                "excellent",
-                client2,
-                null,
-                null
-
-        );
-
-
-        ClientDetailResponseDTO clientDetailResponseDTO1 = new ClientDetailResponseDTO();
-        ClientDetailResponseDTO clientDetailResponseDTO2 = new ClientDetailResponseDTO();
-
-
-
-        List<CommandeEntity> commandes = Arrays.asList(commande1,commande2);
-
-
-        when(commandeComponent.findAllCommandes()).thenReturn(commandes);
-        when(commandeMapper.toCommandesResponseDTO(commandes)).thenReturn(Arrays.asList(
-               new CommandeResponseDTO("1ABC",null,null,clientDetailResponseDTO1),
-                new CommandeResponseDTO("1BC",null,null,clientDetailResponseDTO2)
-        ));
-
-        // Exécution
-        List<CommandeResponseDTO> result = commandeService.getCommandes(null);
-
-        // Vérifications
-        assertNotNull(result, "La liste de résultats ne doit pas être nulle");
-        assertEquals(2, result.size(), "La liste doit contenir deux éléments");
-        assertEquals("1ABC", result.get(0).getReference(), "La référence de la première commande doit correspondre");
-        assertEquals("1BC", result.get(1).getReference(), "La référence de la deuxième commande doit correspondre");
-
-
-    }
-
-    @Test
-    void getCommandeWithSpecificState() {
-        EtatsDeCommande etatsDeCommande = EtatsDeCommande.LIVREE;
-
-        CommandeEntity commande = new CommandeEntity(
-                "1ABDR",
-                etatsDeCommande,
-                null,
-                6,
-                null,
-                null,
-                null,
-                null
-
-        );
-
-        commande.setEtat(etatsDeCommande);
-
-        List<CommandeEntity> commandes = List.of(commande);
-
-        when(commandeComponent.findCommandByEtat(etatsDeCommande)).thenReturn(commandes);
-
-        when(commandeMapper.toCommandesResponseDTO(commandes)).thenReturn(List.of(
-                new CommandeResponseDTO("1ABDR", etatsDeCommande, null, null)
-        ));
-
-        List<CommandeResponseDTO> result = commandeService.getCommandes(etatsDeCommande);
-
-        assertEquals(1, result.size(), "devrait renvoyer une commande");
-        verify(commandeComponent).findCommandByEtat(etatsDeCommande);
-        assertEquals("1ABDR",result.get(0).getReference());
-        verify(commandeMapper,times(2)).toCommandesResponseDTO(commandes);
-
-
-
-    }
+//    @Test
+//    void getCommandesWithNoState() {
+//
+//       // Given
+//
+//
+//        ClientEntity client1 = new ClientEntity();
+//        ClientEntity client2 = new ClientEntity();
+//
+//
+//        CommandeEntity commande1 = new CommandeEntity(
+//                "1ABC",
+//                null,
+//                null,
+//                10,
+//                "magnifique",
+//                client1,
+//                null,
+//                null
+//        );
+//        CommandeEntity commande2 = new CommandeEntity(
+//                "1BC",
+//                null,
+//                null,
+//                9,
+//                "excellent",
+//                client2,
+//                null,
+//                null
+//
+//        );
+//
+//
+//        ClientDetailResponseDTO clientDetailResponseDTO1 = new ClientDetailResponseDTO();
+//        ClientDetailResponseDTO clientDetailResponseDTO2 = new ClientDetailResponseDTO();
+//
+//
+//
+//        List<CommandeEntity> commandes = Arrays.asList(commande1,commande2);
+//
+//
+//        when(commandeComponent.findAllCommandes()).thenReturn(commandes);
+//        when(commandeMapper.toCommandesResponseDTO(commandes)).thenReturn(Arrays.asList(
+//               new CommandeResponseDTO("1ABC",null,null,clientDetailResponseDTO1),
+//                new CommandeResponseDTO("1BC",null,null,clientDetailResponseDTO2)
+//        ));
+//
+//        // Exécution
+//        List<CommandeResponseDTO> result = commandeService.getCommandes(null);
+//
+//        // Vérifications
+//        assertNotNull(result, "La liste de résultats ne doit pas être nulle");
+//        assertEquals(2, result.size(), "La liste doit contenir deux éléments");
+//        assertEquals("1ABC", result.get(0).getReference(), "La référence de la première commande doit correspondre");
+//        assertEquals("1BC", result.get(1).getReference(), "La référence de la deuxième commande doit correspondre");
+//
+//
+//    }
+//
+//    @Test
+//    void getCommandeWithSpecificState() {
+//        EtatsDeCommande etatsDeCommande = EtatsDeCommande.LIVREE;
+//
+//        CommandeEntity commande = new CommandeEntity(
+//                "1ABDR",
+//                etatsDeCommande,
+//                null,
+//                6,
+//                null,
+//                null,
+//                null,
+//                null
+//
+//        );
+//
+//        commande.setEtat(etatsDeCommande);
+//
+//        List<CommandeEntity> commandes = List.of(commande);
+//
+//        when(commandeComponent.findCommandByEtat(etatsDeCommande)).thenReturn(commandes);
+//
+//        when(commandeMapper.toCommandesResponseDTO(commandes)).thenReturn(List.of(
+//                new CommandeResponseDTO("1ABDR", etatsDeCommande, null, null)
+//        ));
+//
+//        List<CommandeResponseDTO> result = commandeService.getCommandes(etatsDeCommande);
+//
+//        assertEquals(1, result.size(), "devrait renvoyer une commande");
+//        verify(commandeComponent).findCommandByEtat(etatsDeCommande);
+//        assertEquals("1ABDR",result.get(0).getReference());
+//        verify(commandeMapper,times(2)).toCommandesResponseDTO(commandes);
+//
+//
+//
+//    }
 
 //    @Test
 //    void testUpdateCommandes() throws CommandeEntityNotFoundException, LivraisonEntityNotFoundException {
