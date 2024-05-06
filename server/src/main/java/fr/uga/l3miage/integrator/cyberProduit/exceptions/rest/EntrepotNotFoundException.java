db26eaf25b573656d13780b406d4f99a56fc15d2
l3miage-aragaooh <higo.aragao-oliveira-medeiros@etu.univ-grenoble-alpes.fr>
2024-05-06 13:37:35 +0200

    feat: ajouter entrepot lors de la creation d'une journée

---
+++ server/src/main/java/fr/uga/l3miage/integrator/cyberProduit/exceptions/rest/EntrepotNotFoundException.java
@@ -0,0 +1,8 @@
+package fr.uga.l3miage.integrator.cyberProduit.exceptions.rest;
+
+public class EntrepotNotFoundException extends RuntimeException{
+    public EntrepotNotFoundException(String message){
+        super(message);
+    }
+
+}
