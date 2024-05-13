e6b0b247f90bed274018baf45367839d43b5caa9
l3miage-aragaooh <higo.aragao-oliveira-medeiros@etu.univ-grenoble-alpes.fr>
2024-05-13 15:24:47 +0200

    refact modif livraison

---
+++ rest-api/src/main/java/fr/uga/l3miage/integrator/cyberCommandes/response/LivraisonUpdateResponseDTO.java
@@ -0,0 +1,26 @@
+package fr.uga.l3miage.integrator.cyberCommandes.response;
+
+import fr.uga.l3miage.integrator.cyberCommandes.enums.EtatsDeLivraison;
+import fr.uga.l3miage.integrator.cyberVitrine.response.CommandeResponseDTO;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Schema(description = "Representation d'une livraison")
+public class LivraisonUpdateResponseDTO {
+    private String reference;
+    private EtatsDeLivraison etat;
+    private Integer tdpEffectif;
+    private Integer tdcEffectif;
+    private Integer tecEffectif;
+    private Integer tdmEffectif;
+    private Set<CommandeResponseDTO> commandes;
+}
