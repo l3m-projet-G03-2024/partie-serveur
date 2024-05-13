0c03a33eb190a554b2463cbabc266ceb61d54446
khalil <ibrahim-goukouni.khalil@etu.univ-grenoble-alpes.fr>
2024-05-13 11:57:51 +0200

    mise à jour du test updateCommande

--- server/src/main/java/fr/uga/l3miage/integrator/cyberCommandes/mappers/TourneeMapper.java
+++ server/src/main/java/fr/uga/l3miage/integrator/cyberCommandes/mappers/TourneeMapper.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import fr.uga.l3miage.integrator.cyberCommandes.request.TourneeCreationRequest;
 import fr.uga.l3miage.integrator.cyberCommandes.response.AddCamionOnTourneeResponseDTO;
-import fr.uga.l3miage.integrator.cyberRessources.mappers.EmployeMapper;
 import fr.uga.l3miage.integrator.cyberVitrine.mappers.CommandeMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -13,7 +12,7 @@ import fr.uga.l3miage.integrator.cyberCommandes.models.TourneeEntity;
 import fr.uga.l3miage.integrator.cyberCommandes.response.TourneeResponseDTO;;;
 
 
-@Mapper(uses = CommandeMapper.class)
+@Mapper(uses = {CommandeMapper.class, JourneeMapper.class})
 public interface TourneeMapper {
 
 
