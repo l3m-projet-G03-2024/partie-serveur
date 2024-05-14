a6a082a0e352d2fc425be4a26f9638e2d33fd659
khalil <ibrahim-goukouni.khalil@etu.univ-grenoble-alpes.fr>
2024-05-14 14:15:15 +0200

    Tests du service et controller de l'endpoint modifier une tournée

--- rest-api/src/main/java/fr/uga/l3miage/integrator/cyberCommandes/request/UpdatingEtatAndTdrEffectifOfTourneeRequest.java
+++ rest-api/src/main/java/fr/uga/l3miage/integrator/cyberCommandes/request/UpdatingEtatAndTdrEffectifOfTourneeRequest.java
@@ -2,17 +2,24 @@ package fr.uga.l3miage.integrator.cyberCommandes.request;
 
 import fr.uga.l3miage.integrator.cyberCommandes.enums.EtatsDeTournee;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @Schema(description = "Requête de mise à jour des attributs etat et tdrEffectif d'une tournée")
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdatingEtatAndTdrEffectifOfTourneeRequest {
 
+    @Schema(description = "la référence de la tournée")
+    private  String referenceTournee ;
+
     @Schema(description = "la nouvelle état de la tournée")
-    private final EtatsDeTournee etat ;
+    private  EtatsDeTournee etat ;
 
     @Schema(description = "le temps effectif de retour de la tournée")
-    private final Integer tdrEffectf ;
+    private  Integer tdrEffectf ;
 }
